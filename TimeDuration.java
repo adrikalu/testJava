67b4416269c8537d6d0801d705a80b0dff492af5
adrikalu <47831108+adrikalu@users.noreply.github.com>
2019-02-21 16:22:50 +0100

    Update TimeDuration.java

--- TimeDuration.java
+++ TimeDuration.java
@@ -5,14 +5,11 @@ public class TimeDuration {
 	private int hours;
 	private int minutes;
 	private int seconds;
-	private int remainder;
 
 	public TimeDuration(int duration) {
 		this.hours = duration / 3600;
-		this.remainder = duration - hours * 3600;
-		this.minutes = remainder / 60;
-		this.remainder = remainder - minutes * 60;
-		this.seconds = remainder;
+		this.minutes = (duration % 3600) / 60;
+		this.seconds = (duration % 3600) % 60;
 	}
 
 	@Override
